0e1cc4b4e96b644f48e31c428f4ddce90005e9ef
Leann Abellanosa <labellanosa@luc.edu>
2024-02-15 23:16:45 +0000

    updated token.h

--- tokens/token.h
+++ tokens/token.h
@@ -1,11 +1,14 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <stdio.h>
 
 typedef enum {
-    TOKEN_NUMBER, 
-    TOKEN_OPERATOR
-    TOKEN_SYMBOL,
-    TOKEN_WORD
+    NUMBER,
+    OPERATOR,
+    SYMBOL,
+    WORD,
+    END_OF_INPUT,
+    UNKNOWN
 } token_type_t;
 
 typedef struct {
@@ -13,9 +16,8 @@ typedef struct {
     char* text; 
 } token_t;
 
-// declaration functions
-
-token_t* create_token(token_type_t, const char* text);
-void free_token(token_t* token);
+token_t get_next_token(FILE* stream);
+void print_token(const token_t token);
+void free_token(token_t* token)
 
 #endif
